4e4536a8ed649f2049142ba18d010d031a71588f
Jokerjay <578055568@qq.com>
2024-05-11 16:03:23 +0800

    新增线程数宏定义 修改个别最大数量

--- include/param.h
+++ include/param.h
@@ -2,7 +2,8 @@
 #define PARAM_H_
 
 // #define DEBUG
-#define MAX_EVENTS 100000     // epoll最大监听事件数量
-#define MAX_CLIENT_NUM 20010  // 最大客户端数量
-#define MAX_MESSAGE_LEN 128 * 1024
+#define MAX_EVENTS 20010            // epoll最大监听事件数量
+#define MAX_CLIENT_NUM 20010        // 最大客户端数量
+#define MAX_MESSAGE_LEN 128 * 1024  // 最大报文长度
+#define THREAD_NUM 8                // 线程数
 #endif
